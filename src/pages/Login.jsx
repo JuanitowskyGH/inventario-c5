47f4231bc17b69162d07da97301cfc8e386352ea
JuanitowskyGH <20206884@garzas.uatx.mx>
2024-07-10 15:01:34 -0600

    Dashboard y Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import InventoryIcon from '../icons/InventoryIcon';
+import UserIcon from '../icons/UserIcon';
+import PasswordIcon from '../icons/PasswordIcon';
+import { LoginForm } from '../components/LoginForm';
 export const Login = () => {
   return (
     <div className="-bottom-56 min-h-screen">
@@ -7,41 +11,10 @@ export const Login = () => {
         <img
           src="/tlx.png"
           alt="Logo"
-          className="w-40 h-50 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+          className=" text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white
+          max-w-xs"
         />
-        <form className="mt-8 bg-white p-6 rounded-lg shadow-md w-96">
-          <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   );
