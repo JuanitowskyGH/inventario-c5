54c9395ee889d264f7b23b9b45598a6f83322278
JuanitowskyGH <20206884@garzas.uatx.mx>
2024-07-11 14:57:12 -0600

    Se ajusto el formato

--- src/components/InventarioForm.jsx
+++ src/components/InventarioForm.jsx
@@ -42,7 +42,7 @@ export const InventarioForm = () => {
                 <input type="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
             </div>
         </div>
-                <div className="mb-5 row-span-3">
+        <div className="mb-5 row-span-3">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripcion</label>
                     <textarea type="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                 </div>
