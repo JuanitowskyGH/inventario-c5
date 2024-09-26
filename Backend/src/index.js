3e8f77391c0e8e8f92cf53292d520d7157bfe469
JuanitowskyGH <20206884@garzas.uatx.mx>
2024-09-26 12:23:07 -0600

    RESOLVER EL PROBLEMA DE HTTPS CON HELMET Y LAS IMAGENES

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const config = require("./config.js");
 const db = require("../src/database/database.js");
+const helmet = require("helmet");
 
 //RUTAS DE LOS MODULOS
 const registers = require("./modules/registers/routes/register.routes.js");
@@ -13,11 +14,32 @@ const { auth } = require("./modules/auth/middleware/auth.middleware.js");
 
 //MIDDLEWARE
 const app = express();
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", 'data:', 'https:', 'http:', 'http://localhost:4000'],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      connectSrc: ["'self'", 'http://localhost:4000'],
+      fontSrc: ["'self'", 'https:', 'http:'],
+      objectSrc: ["'none'"],
+      mediaSrc: ["'self'", 'http://localhost:4000'],
+      frameSrc: ["'self'"]
+    }
+  }
+}));
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin"
+  );
+  next();
+});
 
 //RUTAS
 app.use("/api/login", public);
