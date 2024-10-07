00c9264be4e59f0da9ffe4fb080c75725998690d
JuanitowskyGH <20206884@garzas.uatx.mx>
2024-10-07 12:07:23 -0600

    COMENZANDO A IMPLEMENTAR MIGRACIONES Y SEEDERS

--- Backend/src/migrations/20241007174942-create-users.js
+++ Backend/src/migrations/20241007174942-create-users.js
@@ -2,21 +2,65 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      apellidop: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      apellidom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imagen: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      roleId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'roles',
+          key: "id",
+        },
+      },
+      createdBy: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'users',
+          key: "id",
+        },
+      },
+      createdAt: {
+        type: DataTypes.DATEONLY,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATEONLY,
+        defaultValue: DataTypes.NOW,
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
   }
 };
